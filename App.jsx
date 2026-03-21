05f49280195b66b91d07fda8063f1c470031b682
Veesha Thaker <vroyal1311@gmail.com>
2026-03-21 17:53:12 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,51 +1,42 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
 import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AdminContext } from './context/AdminContext';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import { Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Admin/Dashboard';
-import AllApointments from './pages/Admin/AllApointments';
-import AddDoctor from './pages/Admin/AddDoctor';
-import DoctorsList from './pages/Admin/DoctorsList';
-import { DoctorContext } from './context/DoctorContext';
-import DoctorDashboard from './pages/Doctor/DoctorDashboard';
-import DoctorAppointments from './pages/Doctor/DoctorAppointments';
-import DoctorProfile from './pages/Doctor/DoctorProfile';
+
+
 
 const App = () => {
-      
-  const {aToken} = useContext(AdminContext)
-  const {dToken} = useContext(DoctorContext)
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
 
-  return aToken || dToken ? (
-    <div className='bg-[#F8F9FD]'>
-        <ToastContainer />
-        <Navbar />
-        <div className='flex items-start'>
-          <Sidebar />
-          <Routes>
-            {/* Admin Route */}
-            <Route path='/' element={<></>}/>
-            <Route path='/admin-dashboard' element={<Dashboard/>}/>
-            <Route path='/all-appointments' element={<AllApointments/>}/>
-            <Route path='/add-doctor' element={<AddDoctor/>}/>
-            <Route path='/doctor-list' element={<DoctorsList/>}/>
-             {/* Doctor Route */}
-            <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
-            <Route path='/doctor-appointments' element={<DoctorAppointments/>}/>
-            <Route path='/doctor-profile' element={<DoctorProfile/>}/>
-          </Routes>
-        </div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/my-appointments' element={<MyAppointments/>} />
+        <Route path='/appointment/:docId' element={<Appointment/>} />
+
+      </Routes>
+      <Footer />
     </div>
-  ) : (
-    <>
-    <Login />
-       <ToastContainer />
-    </>
   )
 }
 
 export default App
+
